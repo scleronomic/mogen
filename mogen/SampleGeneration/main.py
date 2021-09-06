0672b6793ee53c062a6c5974402949976727b770
Johannes Tenhumberg <johannes.tenhumberg@gmail.com>
2021-09-06 14:12:16 +0200

    SingleSphere_100-1000

--- mogen/SampleGeneration/main.py
+++ mogen/SampleGeneration/main.py
@@ -124,7 +124,7 @@ def main(iw0=None, iw1=None):
         return sample_path(gen=gen, i_world=_i_w, i_sample=_i_s, img_cmp=worlds[_i_w])
 
     futures = []
-    for i_w in range(0, 100):
+    for i_w in range(100, 1000):
         for i_s in range(n_samples_per_world):
             futures.append(sample_ray.remote(i_w, i_s))
 
@@ -134,7 +134,7 @@ def main(iw0=None, iw1=None):
     for df_i in df_list[1:]:
         df = df.append(df_i)
 
-    df2sql(df=df, file=db_file, table='paths', if_exists='replace')
+    df2sql(df=df, file=db_file, table='paths', if_exists='append')
     print(df)
     return df
 
