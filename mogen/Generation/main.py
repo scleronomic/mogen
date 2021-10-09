67317614e4fc95c176cb83c6a0a3c2ea4ad0bc6e
Johannes Tenhumberg <johannes.tenhumberg@gmail.com>
2021-10-09 08:48:05 +0200

    SingleSphere02 1 - 10000

--- mogen/Generation/main.py
+++ mogen/Generation/main.py
@@ -166,23 +166,23 @@ def main(iw_list=None):
         df = df.append(df_i)
 
     tic()
-    df2sql(df=df, file=db_file, table='paths', if_exists='replace')
+    df2sql(df=df, file=db_file, table='paths', if_exists='append')
     toc(f'Time for appending {len(df)} rows')
     print(df)
     return df
 
 
 def meta_main():
-    worlds = np.arange(3, 10000)
-    for iw in np.array_split(worlds, 40):
+    worlds = np.arange(1, 10000)
+    for iw in np.array_split(worlds, 50):
         main(iw)
 
 
 if __name__ == '__main__':
     from wzk import tic, toc
     tic()
-    # meta_main()
-    df = main(iw_list=np.arange(1))
+    meta_main()
+    # df = main(iw_list=np.arange(1))
     toc()
     # print('New DB:', get_n_rows(file=db_file, table='paths'))
 
