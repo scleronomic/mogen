03b529a8779f61ce81a39c0ec9784f0f82f9fb70
Johannes Tenhumberg <johannes.tenhumberg@gmail.com>
2021-10-09 22:01:24 +0200

    test save time

--- mogen/Generation/test_save.py
+++ mogen/Generation/test_save.py
@@ -6,10 +6,10 @@ from mogen.Loading.load_sql import df2sql
 
 fileA = '/volume/USERSTORE/tenh_jo/A.db'
 fileB = '/net/rmc-lx0062/home_local/tenh_jo/B.db'
-fileC = '/home_local/tenh_jo/B.db'
+fileC = '/home_local/tenh_jo/C.db'
 
 
-n = 10000
+n = 100000
 i_world = np.arange(n)
 i_sample = np.arange(n)
 f = np.ones(n,dtype=bool)
@@ -21,14 +21,14 @@ df = create_path_df(i_world=i_world, i_sample=i_sample, q0=q0, q=q, objective=o,
 
 if_exists = 'replace'
 print(n)
-tic()
-df2sql(df=df, file=fileA, table='paths', if_exists=if_exists)
-toc('userstore')
+# tic()
+# df2sql(df=df, file=fileA, table='paths', if_exists=if_exists)
+# toc('userstore')
 
 tic()
 df2sql(df=df, file=fileB, table='paths', if_exists=if_exists)
 toc('home_local')
 
-tic()
-df2sql(df=df, file=fileC, table='paths', if_exists=if_exists)
-toc('home_local')
+# tic()
+# df2sql(df=df, file=fileC, table='paths', if_exists=if_exists)
+# toc('home_local')
