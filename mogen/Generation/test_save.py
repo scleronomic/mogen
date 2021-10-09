ba42c258d738381d2b6d4d644cb9f69b210d118e
Johannes Tenhumberg <johannes.tenhumberg@gmail.com>
2021-10-09 12:11:31 +0200

    test save time

---
+++ mogen/Generation/test_save.py
@@ -0,0 +1,26 @@
+import numpy as np
+from wzk import tic, toc
+
+from mogen.Loading.load_pandas import create_path_df
+from mogen.Loading.load_sql import df2sql
+
+fileA = f'/volume/USERSTORE/tenh_jo/A.db'
+fileB = '/net/rmc-lx0062/home_local/tenh_jo/B.db'
+
+
+n = 10000
+i_world = np.range(n)
+i_sample = np.range(n)
+f = np.ones(n,dtype=bool)
+o = np.random.random(n)
+q = np.random.random((n, 20, 10))
+q0 = np.random.random((n, 20, 10))
+df = create_path_df(i_world=i_world, i_sample=i_sample, q0=q0, q=q, objective=o, feasible=f)
+
+tic()
+df2sql(df=df, file=fileA, table='paths')
+toc('userstore')
+
+tic()
+df2sql(df=df, file=fileB, table='paths')
+toc('home_local')
