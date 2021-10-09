40956f2dbea33dab07222749b0c2f916d30141c1
Johannes Tenhumberg <johannes.tenhumberg@gmail.com>
2021-10-09 12:18:16 +0200

    test save time

--- mogen/Generation/test_save.py
+++ mogen/Generation/test_save.py
@@ -18,15 +18,17 @@ q = np.random.random((n, 20, 10))
 q0 = np.random.random((n, 20, 10))
 df = create_path_df(i_world=i_world, i_sample=i_sample, q0=q0, q=q, objective=o, feasible=f)
 
+
+if_exists = 'replace'
 print(n)
 tic()
-df2sql(df=df, file=fileA, table='paths', if_exists='append')
+df2sql(df=df, file=fileA, table='paths', if_exists=if_exists)
 toc('userstore')
 
 tic()
-df2sql(df=df, file=fileB, table='paths', if_exists='append')
+df2sql(df=df, file=fileB, table='paths', if_exists=if_exists)
 toc('home_local')
 
 tic()
-df2sql(df=df, file=fileC, table='paths', if_exists='append')
+df2sql(df=df, file=fileC, table='paths', if_exists=if_exists)
 toc('home_local')
