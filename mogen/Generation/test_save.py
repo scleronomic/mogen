189a22ba504ec0e397e3b2f1170779ceea31f064
Johannes Tenhumberg <johannes.tenhumberg@gmail.com>
2021-10-09 12:13:38 +0200

    test save time

--- mogen/Generation/test_save.py
+++ mogen/Generation/test_save.py
@@ -5,7 +5,7 @@ from mogen.Loading.load_pandas import create_path_df
 from mogen.Loading.load_sql import df2sql
 
 fileA = f'/volume/USERSTORE/tenh_jo/A.db'
-fileB = '/net/rmc-lx0062/home_local/tenh_jo/B.db'
+fileB = '/rmc-lx0062/home_local/tenh_jo/B.db'
 
 
 n = 10000
@@ -17,6 +17,7 @@ q = np.random.random((n, 20, 10))
 q0 = np.random.random((n, 20, 10))
 df = create_path_df(i_world=i_world, i_sample=i_sample, q0=q0, q=q, objective=o, feasible=f)
 
+print(n)
 tic()
 df2sql(df=df, file=fileA, table='paths')
 toc('userstore')
