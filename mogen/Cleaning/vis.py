a3b7f3809f1fcac64d385640649b4f4a25976e78
Johannes Tenhumberg <johannes.tenhumberg@gmail.com>
2021-11-01 16:59:32 +0100

    SingleSphere02 -> easy + hard

--- mogen/Cleaning/vis.py
+++ mogen/Cleaning/vis.py
@@ -4,23 +4,43 @@ from wzk.sql2 import get_values_sql
 from wzk.mpl import new_fig, save_fig
 from wzk.image import compressed2img
 
-from rokin.Vis import robot_3d
+from rokin.Vis import robot_2d, robot_3d
 from rokin.Robots import *
 from mopla.parameter import Parameter
 
 
-def sample_gif_3d(i_s, file):
-
-    robot = Justin19()
-    par = Parameter(robot=robot)
-
+def get_sample(file, i_s, img_shape):
     i_w, i_s, q = get_values_sql(file=file, table='paths',
                                  rows=i_s, columns=['world_i32', 'sample_i32', 'q_f32'],
                                  values_only=True)
     i_w = int(np.squeeze(i_w))
     i_s = int(np.squeeze(i_s))
     img_cmp = get_values_sql(file=file, rows=i_w, table='worlds', columns='img_cmp', values_only=True)
-    img = compressed2img(img_cmp=img_cmp, shape=par.world.shape, dtype=bool)
+    img = compressed2img(img_cmp=img_cmp, shape=img_shape, dtype=bool)
+
+    return i_w, i_s, q, img
+
+
+def plot_path_2d(i_s, robot, file):
+
+    par = Parameter(robot=robot)
+    i_w, i_s, q, img = get_sample(file=file, i_s=i_s, img_shape=par.world.shape)
+    file_path = f'/volume/USERSTORE/tenh_jo/0_Data/Samples/{robot.id}_w{i_w}_s{i_s}'
+
+    q = q.reshape(-1, robot.n_dof)
+
+    fig, ax = robot_2d.new_world_fig(limits=par.world.limits)
+    robot_2d.plot_img_patch_w_outlines(ax=ax, img=img, limits=par.world.limits)
+    robot_2d.plot_x_path(x=q, r=par.robot.spheres_rad, ax=ax)
+    save_fig(file=file_path, fig=fig, formats=('pdf', 'png'))
+
+
+def plot_gif_3d(i_s, file):
+
+    robot = Justin19()
+    par = Parameter(robot=robot)
+
+    i_w, i_s, q, img = get_sample(file=file, i_s=i_s, img_shape=par.world.shape)
 
     file_gif = f'/volume/USERSTORE/tenh_jo/0_Data/Samples/{robot.id}_w{i_w}_s{i_s}.gif'
 
@@ -49,12 +69,16 @@ def plot_dist_to_q0(file, robot, i):
     save_fig(fig=fig, file=file_hist, formats=('pdf', 'png'))
 
 
-robot = Justin19()
-file_easy = f'/net/rmc-lx0062/home_local/tenh_jo/{robot.id}_easy.db'
-file_hard = f'/net/rmc-lx0062/home_local/tenh_jo/{robot.id}_hard.db'
+if __name__ == '__main__':
 
+    # robot = Justin19()
+    robot = SingleSphere02()
+    file_easy = f'/net/rmc-lx0062/home_local/tenh_jo/{robot.id}_easy.db'
+    file_hard = f'/net/rmc-lx0062/home_local/tenh_jo/{robot.id}_hard.db'
 
-plot_dist_to_q0(file=file_easy, robot=robot, i=np.arange(10000))
+    # plot_dist_to_q0(file=file_easy, robot=robot, i=np.arange(10000))
+    plot_path_2d(file=file_easy, robot=robot, i_s=0)
+    plot_path_2d(file=file_easy, robot=robot, i_s=1)
 
 
 # q = np.random.random((100, 20, 19))
