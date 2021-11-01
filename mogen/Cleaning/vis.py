271890115c2d13c203faf298fcc02d4b9d517403
Johannes Tenhumberg <johannes.tenhumberg@gmail.com>
2021-11-01 16:43:50 +0100

    SingleSphere02 -> easy + hard

--- mogen/Cleaning/vis.py
+++ mogen/Cleaning/vis.py
@@ -45,7 +45,7 @@ def plot_dist_to_q0(file, robot, i):
     fig, ax = new_fig()
     print(len(dq))
     ax.hist(dq, bins=50)
-    save_fig(fig=fig, file=file_hist, formats='pdf')
+    save_fig(fig=fig, file=file_hist, formats=('pdf', 'png'))
 
 
 robot = Justin19()
