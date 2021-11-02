b639d1601268a660f65f49f698921e7b5af5fa86
Johannes Tenhumberg <johannes.tenhumberg@gmail.com>
2021-11-02 16:34:14 +0100

    test zlib

--- mogen/Generation/worlds.py
+++ mogen/Generation/worlds.py
@@ -131,8 +131,9 @@ def main():
 
 
 def test_zlib():
-    n = 100
-    a = np.random.random((n, 64, 64, 64)) < 0.1
+    n = 1000
+    s = (16, 16, 16)
+    a = np.random.random((n,) + s) < 0.1
     a = a.astype(bool)
     b = img2compressed(img=a, n_dim=3)
     b2 = b.astype(bytes)
@@ -140,22 +141,22 @@ def test_zlib():
         assert bb == bb2
     print('1')
 
-    a2 = compressed2img(img_cmp=b, shape=(64, 64, 64), dtype=bool)
+    a2 = compressed2img(img_cmp=b, shape=s, dtype=bool)
     print('2')
     assert np.allclose(a, a2)
-    a3 = compressed2img(img_cmp=b2, shape=(64, 64, 64), dtype=bool)
+    a3 = compressed2img(img_cmp=b2, shape=s, dtype=bool)
     assert np.allclose(a, a3)
     print('3')
 
     df = create_world_df(i_world=np.arange(n), img_cmp=b)
-    a4 = compressed2img(img_cmp=df.img_cmp.values, shape=(64, 64, 64), dtype=bool)
+    a4 = compressed2img(img_cmp=df.img_cmp.values, shape=s, dtype=bool)
     assert np.allclose(a, a4)
     print('4')
 
     file = f'/net/rmc-lx0062/home_local/tenh_jo/zlib.db'
     df2sql(df=df, file=file, table='worlds', if_exists='replace')
     img = get_values_sql(file=file, table='worlds', columns='img_cmp', values_only=True)
-    a3 = compressed2img(img_cmp=img, shape=(64, 64, 64), dtype=bool)
+    a3 = compressed2img(img_cmp=img, shape=s, dtype=bool)
     print(a3.shape)
     np.allclose(a, a3)
 
