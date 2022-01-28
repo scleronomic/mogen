61cc12c7e6a1fd56aac05ec25ce1ff2b57b077da
Johannes Tenhumberg <johannes.tenhumberg@gmail.com>
2022-01-28 23:37:55 +0100

    bug fix: gsutil copy

--- mogen/cloud/startup/basic.sh
+++ mogen/cloud/startup/basic.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
-python /home/"$GCP_USER"/src/wzk/wzk/git2.py
 
 # mount additional disk
 echo "Sleep... and mount disk... "
 sleep 300
 sudo mkfs.ext4 /dev/sdb
 sudo mount -t ext4 /dev/sdb /home/"$GCP_USER"/sdb
-sudo chmod 744 -R /home/"$GCP_USER"/sdb/
+sudo chmod 777 -R /home/"$GCP_USER"/sdb/
 echo "Sleep... and mount disk... finished"
