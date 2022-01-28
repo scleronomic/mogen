33b4255d80f8db3e6509f8b7c8fc9bc81f924100
Johannes Tenhumberg <johannes.tenhumberg@gmail.com>
2022-01-29 00:01:12 +0100

    increase sleep time

--- mogen/cloud/startup/basic.sh
+++ mogen/cloud/startup/basic.sh
@@ -2,7 +2,7 @@
 
 # mount additional disk
 echo "Sleep... and mount disk... "
-sleep 10
+sleep 300
 sudo mkfs.ext4 /dev/sdb
 sudo mount -t ext4 /dev/sdb /home/"$GCP_USER"/sdb
 sudo chmod 777 -R /home/"$GCP_USER"/sdb/
