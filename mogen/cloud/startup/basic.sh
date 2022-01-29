2a1ac686b6d1ec8d3061a05dfc913fdfe9fe0fad
Johannes Tenhumberg <johannes.tenhumberg@gmail.com>
2022-01-29 02:02:54 +0100

    make new disk +rwx

--- mogen/cloud/startup/basic.sh
+++ mogen/cloud/startup/basic.sh
@@ -5,5 +5,7 @@ echo "Sleep... and mount disk... "
 sleep 300
 sudo mkfs.ext4 /dev/sdb
 sudo mount -t ext4 /dev/sdb /home/"$GCP_USER"/sdb
+sudo chmod 777 -R /home/"$GCP_USER"/sdb
 sudo chmod 777 -R /home/"$GCP_USER"/sdb/
+sudo chmod 777 -R /home/"$GCP_USER"/sdb/*
 echo "Sleep... and mount disk... finished"
