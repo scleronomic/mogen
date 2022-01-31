7c18b79b5e3b2732a19a78f03de2af2f80e16763
Johannes Tenhumberg <johannes.tenhumberg@gmail.com>
2022-01-31 10:16:15 +0100

    combine from bucket 2 bucket

--- mogen/cleaning/clean_data.py
+++ mogen/cleaning/clean_data.py
@@ -294,7 +294,7 @@ def test_separate_easy_hard():
 
 
 def main_combine_files():
-    old_files = [f"gs://tenh_jo/StaticArm04_{i}.db" for i in range(2)]
+    old_files = [f"gs://tenh_jo/StaticArm04_{i}.db" for i in range(20)]
     new_file = '/home/johannes_tenhumberg/sdb/StaticArm04_combined.db'
     combine_files(old_files=old_files, new_file=new_file)
 
