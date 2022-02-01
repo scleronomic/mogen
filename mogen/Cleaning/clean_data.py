7b12b499f559ae9dbb0340e0a1afb415b9ecac74
Johannes Tenhumberg <johannes.tenhumberg@gmail.com>
2022-02-01 14:26:14 +0100

    search bug, memory error

--- mogen/Cleaning/clean_data.py
+++ mogen/Cleaning/clean_data.py
@@ -220,7 +220,7 @@ def main_separate_easy_hard(file: str):
 
     print(f"Separate {file} into easy and hard")
     print('Copy initial file -> file_easy')
-    sql2.delete_rows(file=file_easy, table=table, rows=np.random.random(int(1e8) < 0.5))
+    sql2.delete_rows(file=file_easy, table=table, rows=np.random.random(int(1e8)) < 0.5)
 
     copy(file, file_easy)
 
