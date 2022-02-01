d39edfd13d4605aec0291032330abc763be56862
Johannes Tenhumberg <johannes.tenhumberg@gmail.com>
2022-02-01 10:25:32 +0100

    new gcloud commands

--- mogen/Cloud/Startup/ompgen.sh
+++ mogen/Cloud/Startup/ompgen.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-source /home/"$GCP_USER"/src/mogen/mogen/cloud/startup/basic.sh
+source /home/"$GCP_USER"/src/mogen/mogen/Cloud/Startup/basic.sh
 
 python /home/"$GCP_USER"/src/mogen/mogen/Generation/paths.py
-
