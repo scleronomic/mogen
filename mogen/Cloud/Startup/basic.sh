f423b718dd936b5e27fc843f4fa9725434931083
Johannes Tenhumberg <johannes.tenhumberg@gmail.com>
2022-02-02 10:51:24 +0100

    search bug, memory error

--- mogen/Cloud/Startup/basic.sh
+++ mogen/Cloud/Startup/basic.sh
@@ -3,7 +3,7 @@
 # mount additional disk
 echo "Sleep... and mount disk... "
 sleep 200
-umask 0777
+# umask 0777
 # https://cloud.google.com/compute/docs/disks/add-persistent-disk#formatting
 sudo mkfs.ext4 -m 0 -E lazy_itable_init=0,lazy_journal_init=0,discard /dev/sdb
 sudo mkdir -p /home/"$GCP_USER"/sdb
