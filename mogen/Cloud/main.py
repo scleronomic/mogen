11d380dd3251bbbd89e11b3cc9315a9f11ca3cee
Johannes Tenhumberg <johannes.tenhumberg@gmail.com>
2022-02-08 00:56:42 +0100

    new cli interface

--- mogen/Cloud/main.py
+++ mogen/Cloud/main.py
@@ -6,7 +6,7 @@ from wzk.gcp import startup
 
 def mogen_create_instances_and_start(name='ompgen', n=10, n0=0, sleep=600):
     machine = 'c2-standard-60'
-    snapshot = 'tenh-setup'
+    snapshot = 'tenh-setup-cpu'
     snapshot_size = 30
     disk_size = 20
     startup_script = startup.make_startup_file(user=GCP_USER,
