355e7f43604e2550ed6cb39d87acf7d72c75a6f4
Johannes Tenhumberg <johannes.tenhumberg@gmail.com>
2022-07-26 11:04:29 +0200

    delete infeasible Justin19

--- mogen/Cleaning/clean.py
+++ mogen/Cleaning/clean.py
@@ -3,17 +3,19 @@ from wzk import sql2
 
 from mogen.Generation import data, parameter
 
+T_PATHS = data.T_PATHS
+
 
 def sort(file):
     print(f'Sort: {file}')
 
-    sql2.sort_table(file=file, table=data.T_PATHS(),
-                    order_by=[data.T_PATHS.C_WORLD_I(), data.T_PATHS.C_SAMPLE_I(), 'ROWID'])
+    sql2.sort_table(file=file, table=T_PATHS(),
+                    order_by=[T_PATHS.C_WORLD_I(), T_PATHS.C_SAMPLE_I(), 'ROWID'])
 
 
 def reset_sample_i32(file):
     print(f'Reset indices: {file}')
-    iw_all = sql2.get_values_sql(file=file, table=data.T_PATHS(), rows=-1, columns=[data.T_PATHS.C_WORLD_I()])
+    iw_all = sql2.get_values_sql(file=file, table=T_PATHS(), rows=-1, columns=[T_PATHS.C_WORLD_I()])
     iw_all = np.squeeze(iw_all)
     n = len(iw_all)
     i_s = np.full(n, -1, dtype=int)
@@ -22,16 +24,16 @@ def reset_sample_i32(file):
         j = np.nonzero(iw_all == iw_i)[0]
         i_s[j] = np.arange(len(j))
 
-    sql2.set_values_sql(file=file, table=data.T_PATHS(), values=(i_s,), columns=data.T_PATHS.C_SAMPLE_I())
+    sql2.set_values_sql(file=file, table=T_PATHS(), values=(i_s,), columns=T_PATHS.C_SAMPLE_I())
 
 
 def reset_sample_i32_0(file):
     print(f"Reset sample_i32 0: {file}")
-    table = data.T_PATHS()
+    table = T_PATHS()
 
     print('Load indices')
     w, s = sql2.get_values_sql(file=file, table=table, rows=-1,
-                               columns=[data.T_PATHS.C_WORLD_I(), data.T_PATHS.C_SAMPLE_I()])
+                               columns=[T_PATHS.C_WORLD_I(), T_PATHS.C_SAMPLE_I()])
     w = np.squeeze(w).astype(np.int32)
     s = np.squeeze(s).astype(np.int32)
     assert np.all(s == 0)
@@ -45,16 +47,16 @@ def reset_sample_i32_0(file):
         s[wb0_i:] += 1
 
     print('Set indices')
-    sql2.set_values_sql(file=file, table=table, values=(s,), columns=data.T_PATHS.C_SAMPLE_I())
+    sql2.set_values_sql(file=file, table=table, values=(s,), columns=T_PATHS.C_SAMPLE_I())
 
 
 def remove_infeasible(file):
-    f = sql2.get_values_sql(file=file, table=data.T_PATHS(), rows=-1, columns=[data.T_PATHS.C_FEASIBLE_I()])
+    f = sql2.get_values_sql(file=file, table=T_PATHS(), rows=-1, columns=[T_PATHS.C_FEASIBLE_I()])
     print(np.unique(f, return_counts=True))
     feasible = f == +1
     print(f"feasible {feasible.sum()}/{feasible.size} ~ {np.round(feasible.mean(), 3)}")
     if not np.all(feasible):
-        sql2.delete_rows(file=file, table=data.T_PATHS, rows=~f)
+        sql2.delete_rows(file=file, table=T_PATHS(), rows=~f)
         reset_sample_i32(file=file)
 
 
@@ -70,9 +72,9 @@ def update_cast_joint_errors(q, limits, eps=1e-6):
 def set_dtypes(file):
     print(f'set dtypes {file}')
     # paths
-    columns_paths_old = sql2.get_columns(file=file, table=data.T_PATHS())
-    columns_paths_new = [data.T_PATHS.C_WORLD_I(), data.T_PATHS.C_SAMPLE_I(),
-                         data.T_PATHS.C_Q_F(), data.T_PATHS.C_OBJECTIVE_F(), data.T_PATHS.C_FEASIBLE_I()]
+    columns_paths_old = sql2.get_columns(file=file, table=T_PATHS())
+    columns_paths_new = [T_PATHS.C_WORLD_I(), T_PATHS.C_SAMPLE_I(),
+                         T_PATHS.C_Q_F(), T_PATHS.C_OBJECTIVE_F(), T_PATHS.C_FEASIBLE_I()]
     dtypes_paths_new = [sql2.TYPE_INTEGER, sql2.TYPE_INTEGER, sql2.TYPE_BLOB, sql2.TYPE_REAL, sql2.TYPE_INTEGER]
     assert np.all(columns_paths_old.name.values == columns_paths_new), f"{columns_paths_old.name.values} \n {columns_paths_new}"
     sql2.alter_table(file, table='paths', columns=columns_paths_new, dtypes=dtypes_paths_new)
